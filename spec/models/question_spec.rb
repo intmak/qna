f8e02e278d59dd5cae2f753f080c96e51029b6ad
Your Name <you@example.com>
2016-02-25 13:00:35 -0500

    shoulda matchers

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+
+it {should validates_presence_of :title }
+    it 'validates presence of title' do
+    expect(Question.new(body:'123')).to_not be_valid
+    end
+    it 'validates presence of body' do
+    expect(Question.new(title:'123')).to_not be_valid
+    end
+
+end
